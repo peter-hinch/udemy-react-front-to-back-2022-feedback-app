5f9d30a9ef1ee3c5ab5335bbc717d650257c66a5
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-07 15:52:41 +1100

    Add FeedbackItem component with state.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
       <Header text="Hello World"/>
+      <div className="container">
+        <FeedbackItem />  
+      </div>
     </>
   )
 }
