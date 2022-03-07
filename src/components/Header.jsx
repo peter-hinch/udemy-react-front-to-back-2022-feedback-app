89e3ff9324aa032a475092e7be45d5eb78305d0b
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-07 15:39:20 +1100

    Add additional styling to Header with default values.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types';
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor
+  }
+
   return (
-    <div className="container">
-      <h2>{text}</h2>
-    </div>
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
   )
 }
 
 Header.defaultProps = {
-  text: 'Feedback UI'
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95'
 }
 
 Header.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string
 }
 
 export default Header
