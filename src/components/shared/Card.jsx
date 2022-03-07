110c0ffea6e0fe457f8fe7c2ef71ddce404023c2
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-07 17:16:31 +1100

    Create Card component.

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,7 +1,31 @@
-function Card({ children }) {
+import PropTypes from 'prop-types';
+
+function Card({ children, reverse }) {
+  // Example of conditional class.
+  // return (
+  //   <div className={`card ${reverse && "reverse"}`}>
+  //     {children}
+  //   </div>
+  // )
+
+  // Example of conditional styling.
   return (
-    <div className="card">{children}</div>
+    <div className="card" style={{
+      backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+      color: reverse ? '#fff' : '#000'
+    }}>
+      {children}
+    </div>
   )
+}
+
+Card.defaultProps = {
+  reverse: false
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool
 }
 
 export default Card
