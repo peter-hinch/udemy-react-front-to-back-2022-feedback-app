9b2a9ae806860434544519af4e3a675684157caa
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-07 16:14:04 +1100

    Add FeedbackData to App state.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/feedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header text="Hello World"/>
       <div className="container">
-        <FeedbackItem />  
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   )
