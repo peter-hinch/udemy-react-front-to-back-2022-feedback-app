9f1cf6eb8e8fb88363fdede425ba5618c89bd865
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-08 11:29:04 +1100

    Improve wording for home Link.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,7 +9,7 @@ function AboutPage() {
         <p>This is a React app to leave feedback for a product or service.</p>
         <p>Version: 1.0.0</p>
         <p>
-          <Link to="/">Back to home</Link>
+          <Link to="/">Return home</Link>
         </p>
       </div>
     </Card>
