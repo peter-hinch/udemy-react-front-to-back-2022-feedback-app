abe9ae6bb2e76f2fb6d2b513230b20109a1fa009
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-09 15:04:27 +1100

    Add visual update of radio button upon edit function.

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react"
+import { useContext, useState, useEffect } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
 function RatingSelect({ select }) {
   const [selected, setSelected] = useState(10);
-  
+
+  // Import the object feedbackEditItem from FeedbackContext.
+  const { feedbackEditItem } = useContext(FeedbackContext);
+
+  useEffect(() => {
+    setSelected(feedbackEditItem.item.rating);
+  }, [feedbackEditItem]);
+
   const handleChange = (event) => {
     let eventVal = +event.currentTarget.value; // '+' converts string to number
-    setSelected(eventVal); 
+    setSelected(eventVal);
     select(eventVal);
-  }
+  };
 
   return (
     <ul className="rating">
       {/* Use Array.from() to generate 10 radio buttons */}
       {/* Reference: https://github.com/bradtraversy/feedback-app/blob/main/src/components/RatingSelect.jsx */}
       {/* Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#using_arrow_functions_and_array.from */}
-      {Array.from({length: 10}, (v, i) => (
+      {Array.from({ length: 10 }, (v, i) => (
         <li>
           <input
             type="radio"
@@ -28,7 +36,7 @@ function RatingSelect({ select }) {
         </li>
       ))}
     </ul>
-  )
+  );
 }
 
-export default RatingSelect
+export default RatingSelect;
