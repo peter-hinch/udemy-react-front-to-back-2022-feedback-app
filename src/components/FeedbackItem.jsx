3bf53a6781eb59b901432951d24f2aec83e87726
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-09 11:46:57 +1100

    Use FeedbackContext to obtain delete function.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,23 +1,26 @@
+import { useContext } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-import Card from "./shared/Card";
+import Card from './shared/Card';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackItem({ item, handleDelete }) {
+function FeedbackItem({ item }) {
+  // Import the deleteFeedback function from FeedbackContext.
+  const { deleteFeedback } = useContext(FeedbackContext);
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
-        <FaTimes color="purple" />  
+      <button onClick={() => deleteFeedback(item.id)} className="close">
+        <FaTimes color="purple" />
       </button>
-      <div className="text-display">
-        {item.text}
-      </div>
+      <div className="text-display">{item.text}</div>
     </Card>
-  )
+  );
 }
 
 FeedbackItem.propTypes = {
   item: PropTypes.object.isRequired
-}
+};
 
-export default FeedbackItem
+export default FeedbackItem;
