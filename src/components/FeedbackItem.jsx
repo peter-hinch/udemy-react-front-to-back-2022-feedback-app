c86be5688919ceddd07bc445f1ed550c7ce55396
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-09 14:27:11 +1100

    Add button for edit functionality.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,21 @@
 import { useContext } from 'react';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackItem({ item }) {
   // Import the deleteFeedback function from FeedbackContext.
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { editFeedback, deleteFeedback } = useContext(FeedbackContext);
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button onClick={() => editFeedback(item)} className="edit">
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
