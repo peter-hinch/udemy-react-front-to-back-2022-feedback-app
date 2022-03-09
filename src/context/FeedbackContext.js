e30b02c618f747ab504ec684603950e3661bd5db
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-03-09 14:17:14 +1100

    Add some dummy data to FeedbackContext.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -8,8 +8,18 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item is from context',
+      text: 'This is feedback item 1',
       rating: 10
+    },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 9
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 7
     }
   ]);
 
